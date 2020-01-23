e229c6bfec181f4361ce282f34314b190ebd2ca7
root <root@ip-10-100-50-97.ec2.internal>
2020-01-23 16:56:52 +0000

    source code

---
+++ src/main/java/com/example/DemoAPP/DemoApplication/Controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.DemoAPP.DemoApplication.Controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public List<String> showData() {
+
+		return Arrays.asList("AA", "BB", "TZ");
+	}
+}
