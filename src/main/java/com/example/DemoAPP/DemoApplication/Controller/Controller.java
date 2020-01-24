8ce1f96242d7509f13a2b9a178de5aaed05ab179
Parthasarathy, V. D. <v.d.parthasarathy@accenture.com>
2020-01-24 10:27:46 +0530

    Commit Changes

--- src/main/java/com/example/DemoAPP/DemoApplication/Controller/Controller.java
+++ src/main/java/com/example/DemoAPP/DemoApplication/Controller/Controller.java
@@ -12,6 +12,6 @@ public class Controller {
 	@GetMapping("/")
 	public List<String> showData() {
 
-		return Arrays.asList("AA", "BB", "testing");
+		return Arrays.asList("AA", "BB", "FFFF");
 	}
 }
